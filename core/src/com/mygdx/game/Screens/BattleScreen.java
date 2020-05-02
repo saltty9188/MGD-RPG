f4bec49990a22868eaba971cf34aee10a0a05110
saltty9188 <62987363+saltty9188@users.noreply.github.com>
2020-05-02 11:48:04 +0930

    Create BattleScreen
    Created the screen that will be used for turn-based battles.

---
+++ core/src/com/mygdx/game/Screens/BattleScreen.java
@@ -0,0 +1,47 @@
+package com.mygdx.game.Screens;
+
+import com.badlogic.gdx.Screen;
+
+public class BattleScreen implements Screen {
+
+    private GameScreen gameScreen;
+
+    public BattleScreen(GameScreen gameScreen) {
+        this.gameScreen = gameScreen;
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
