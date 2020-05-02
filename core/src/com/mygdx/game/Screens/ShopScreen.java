ca7df269c256e45095d8260dd1aade95ae8f5597
saltty9188 <62987363+saltty9188@users.noreply.github.com>
2020-05-02 19:12:09 +0930

    Create ShopScreen
    Created the screen that will be shown when purchasing items

---
+++ core/src/com/mygdx/game/Screens/ShopScreen.java
@@ -0,0 +1,45 @@
+package com.mygdx.game.Screens;
+
+import com.badlogic.gdx.Screen;
+
+/**
+ * This screen is shown when the player talks to the shopkeeper NPC to purchase items.
+ * The details on this screen include the type of item, the price, the quantity being bought,
+ * and the quantity currently in the player's inventory.
+ */
+public class ShopScreen implements Screen {
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
