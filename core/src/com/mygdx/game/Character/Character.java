e0852de41d8749d28aab98debd57b6bc054d49a3
saltty9188 <62987363+saltty9188@users.noreply.github.com>
2020-05-03 15:14:58 +0930

    Create Character
    Created the Character class that will serve as the foundation for all Character's in the game.

---
+++ core/src/com/mygdx/game/Character/Character.java
@@ -0,0 +1,20 @@
+package com.mygdx.game.Character;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+
+/**
+ * The base class for all the Characters of the game.
+ */
+public class Character {
+    /**
+     * The Sprite that will hold the Character's texture and position information.
+     */
+    private Sprite sprite;
+
+    /**
+     * The sprite sheet holding the Character's sprite(s).
+     */
+    private Texture spriteSheet;
+
+}
