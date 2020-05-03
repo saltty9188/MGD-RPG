a8fb8ee60ee677338b8731c482c8d9783d2a41ff
saltty9188 <62987363+saltty9188@users.noreply.github.com>
2020-05-03 15:20:14 +0930

    Create Enemy
    Created the Enemy class.

---
+++ core/src/com/mygdx/game/Character/Enemy.java
@@ -0,0 +1,17 @@
+package com.mygdx.game.Character;
+
+import com.badlogic.gdx.graphics.Texture;
+
+/**
+ * The class that represents the Enemies the Player will face both in the overworld
+ * and in the battle screens.
+ */
+public class Enemy extends Character {
+
+    /**
+     * The sprite used when the Player is battling the Enemy.
+     */
+    private Texture battleSprite;
+
+
+}
