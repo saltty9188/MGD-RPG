0ef7588b304fb6c69d8e621d9f73861492e2bd56
saltty9188 <62987363+saltty9188@users.noreply.github.com>
2020-05-14 13:11:01 +0930

    Add Buttons
    Added a button class with the ability to draw text on the button if needed. Added some action buttons to BattleScreen.

---
+++ core/src/com/mygdx/game/Button.java
@@ -0,0 +1,78 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.GlyphLayout;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class Button {
+
+    float x;
+    float y;
+    float width;
+    float height;
+    boolean isDown;
+
+    Texture upTexture;
+    Texture downTexture;
+
+    private BitmapFont bmfont;
+
+    public Button(float x, float y, float width, float height, Texture up, Texture down) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+
+        this.upTexture = up;
+        this.downTexture = down;
+        isDown = false;
+
+        bmfont = new BitmapFont(
+                Gdx.files.internal("font/good_neighbors.fnt"),
+                Gdx.files.internal("font/good_neighbors.png"),
+                false);
+        bmfont.getData().setScale(2f);
+    }
+
+    public void update(boolean checkTouch, int touchX, int touchY) {
+        isDown = false;
+
+        if(checkTouch) {
+
+            if(touchX >= x && touchX <= x + width && touchY >= y && touchY <= y + height ) {
+                isDown = true;
+            }
+        }
+    }
+
+    public void draw(SpriteBatch batch) {
+        if(isDown) {
+            batch.draw(downTexture, x, y, width, height);
+        }
+        else {
+            batch.draw(upTexture, x, y, width, height);
+        }
+    }
+
+    public void draw(SpriteBatch batch, String text) {
+        GlyphLayout glyphLayout = new GlyphLayout();
+        glyphLayout.setText(bmfont, text);
+
+        int textX = (int) ((width/2 - glyphLayout.width/2) + x);
+        int textY = (int) ((height/2 - glyphLayout.height/2) + y);
+
+        if(isDown) {
+            batch.draw(downTexture, x, y, width, height);
+            bmfont.draw(batch, text, textX, textY-1, glyphLayout.width,
+                    1, true);
+        }
+        else {
+            batch.draw(upTexture, x, y, width, height);
+            bmfont.draw(batch, text, textX, textY, glyphLayout.width,
+                    1, true);
+        }
+
+    }
+}
--- core/src/com/mygdx/game/Screens/BattleScreen.java
+++ core/src/com/mygdx/game/Screens/BattleScreen.java
@@ -1,26 +1,37 @@
 package com.mygdx.game.Screens;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.mygdx.game.Button;
 import com.mygdx.game.Character.Enemy;
 import com.mygdx.game.Character.Player;
 
 public class BattleScreen implements Screen {
 
+    private static final float BUTTON_PAD = 0.5f;
+
     private GameScreen gameScreen;
     private Enemy enemy;
     private Player player;
+
+    private Texture buttonUp;
+    private Texture buttonDown;
 
     SpriteBatch spriteBatch;
 
     private BitmapFont bmfont;
 
     private TextureRegion textWindow;
+
+    private Button attackButton;
+    private Button itemButton;
+    private Button runButton;
 
     public BattleScreen(GameScreen gameScreen, Enemy enemy, Player player) {
         this.gameScreen = gameScreen;
@@ -37,6 +48,16 @@ public class BattleScreen implements Screen {
         bmfont.getData().setScale(2);
 
         textWindow = new TextureRegion(new Texture("window.png"));
+
+        buttonUp = new Texture("buttonUp.png");
+        buttonDown = new Texture("buttonDown.png");
+
+        attackButton = new Button(Gdx.graphics.getWidth()/2 + BUTTON_PAD, textWindow.getRegionHeight()/2 + BUTTON_PAD, Gdx.graphics.getWidth()/2 - 2 * BUTTON_PAD,
+                textWindow.getRegionHeight()/2 - 2 * BUTTON_PAD, buttonUp, buttonDown);
+        itemButton = new Button(Gdx.graphics.getWidth()/2 + BUTTON_PAD, 0, Gdx.graphics.getWidth()/4 - 2 * BUTTON_PAD,
+                textWindow.getRegionHeight()/2 - BUTTON_PAD, buttonUp, buttonDown);
+        runButton = new Button(Gdx.graphics.getWidth() * 3/4 + BUTTON_PAD, 0, Gdx.graphics.getWidth()/4 - 2 * BUTTON_PAD,
+                textWindow.getRegionHeight()/2 - BUTTON_PAD, buttonUp, buttonDown);
     }
 
     @Override
@@ -58,11 +79,25 @@ public class BattleScreen implements Screen {
         spriteBatch.draw(textWindow, 0, 0);
         bmfont.draw(spriteBatch, "How will you proceed?", Gdx.graphics.getWidth() * 0.034f, Gdx.graphics.getHeight() * 0.12f, Gdx.graphics.getWidth() * 0.43f,
                 1, true);
+
+        attackButton.draw(spriteBatch, "Attack");
+        itemButton.draw(spriteBatch, "Items");
+        runButton.draw(spriteBatch, "Run Away");
+
         spriteBatch.end();
     }
 
     private void update() {
-        if(Gdx.input.isTouched()) gameScreen.game.setScreen(gameScreen);
+        boolean checkTouch = Gdx.input.isTouched();
+
+        int touchX = Gdx.input.getX();
+        int touchY = Gdx.graphics.getHeight() - Gdx.input.getY();
+
+        attackButton.update(checkTouch, touchX, touchY);
+        itemButton.update(checkTouch, touchX, touchY);
+        runButton.update(checkTouch, touchX, touchY);
+
+        if(Gdx.input.isKeyPressed(Input.Keys.ESCAPE)) gameScreen.game.setScreen(gameScreen);
     }
 
     @Override
@@ -88,5 +123,6 @@ public class BattleScreen implements Screen {
     @Override
     public void dispose() {
         bmfont.dispose();
+        textWindow.getTexture().dispose();
     }
 }
