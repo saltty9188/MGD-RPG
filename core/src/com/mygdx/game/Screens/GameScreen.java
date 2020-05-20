a6d0c55a02766e5f26a46e22028ac9da5eca0b75
tom-whyatt <55425421+tom-whyatt@users.noreply.github.com>
2020-05-20 15:02:01 +0930

    Committed unneeded changes

--- core/src/com/mygdx/game/Screens/GameScreen.java
+++ core/src/com/mygdx/game/Screens/GameScreen.java
@@ -8,9 +8,28 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+<<<<<<< Updated upstream
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.utils.viewport.FitViewport;
+=======
+import com.badlogic.gdx.maps.MapLayer;
+import com.badlogic.gdx.maps.objects.RectangleMapObject;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.mygdx.game.Character.Player;
+import com.mygdx.game.WorldAnimations.Flag;
+import com.mygdx.game.WorldAnimations.Fountain;
+>>>>>>> Stashed changes
 import com.mygdx.game.RPGGame;
+
+import java.util.ArrayList;
 
 public class GameScreen implements Screen {
 
@@ -18,15 +37,40 @@ public class GameScreen implements Screen {
     private RPGGame game;
 
     private SpriteBatch spriteBatch;
+<<<<<<< Updated upstream
 
     private Texture playerSheet;
     private TextureRegion walkDown,walkRight,walkUp,walkLeft;
     private Animation<TextureRegion> animation;
     private Sprite player;
+=======
+    private Player player;
+    private Fountain fountain;
+    private Flag flag1, flag2, flag3;
+    private ArrayList<Flag> flags;
+    private ArrayList<RectangleMapObject> objects;
+>>>>>>> Stashed changes
     private OrthographicCamera gameCam;
     private FitViewport gamePort;
 
     public GameScreen(RPGGame game) {
+<<<<<<< Updated upstream
+=======
+        this.game = game;
+    }
+
+    private void create() {
+        spriteBatch = new SpriteBatch();
+
+        flags = new ArrayList<Flag>();
+        objects = new ArrayList<RectangleMapObject>();
+
+        flags.add(flag1);
+        flags.add(flag2);
+        flags.add(flag3);
+
+        TmxMapLoader temp = new TmxMapLoader();
+>>>>>>> Stashed changes
 
         this.game = game;
 
@@ -47,6 +91,21 @@ public class GameScreen implements Screen {
 
         player.setSize(64,128);
 
+<<<<<<< Updated upstream
+=======
+        MapLayer objectLayer = map.getLayers().get("Spawns");
+        RectangleMapObject playerSpawn = (RectangleMapObject)objectLayer.getObjects().get("Player");
+        RectangleMapObject fountainSpawn = (RectangleMapObject)objectLayer.getObjects().get("Fountain");
+        RectangleMapObject flag1Spawn = (RectangleMapObject)objectLayer.getObjects().get("Flag1");
+        RectangleMapObject flag2Spawn = (RectangleMapObject)objectLayer.getObjects().get("Flag2");
+        RectangleMapObject flag3Spawn = (RectangleMapObject)objectLayer.getObjects().get("Flag3");
+        player.setCenter(playerSpawn.getRectangle().x, playerSpawn.getRectangle().y);
+        fountain.setCenter(fountainSpawn.getRectangle().x, fountainSpawn.getRectangle().y);
+        for(int i = 0; i < flags.size(); i++) {
+            RectangleMapObject obj = (RectangleMapObject)objectLayer.getObjects().get("Flag" + i);
+            flags.get(i).setCenter(obj.getRectangle().x, obj.getRectangle().y);
+        }
+>>>>>>> Stashed changes
         gameCam.position.set(player.getX(), player.getY(), 0);
     }
 
@@ -56,6 +115,14 @@ public class GameScreen implements Screen {
     public void update(float delta){
         gameCam.position.x = player.getX();
         gameCam.update();
+<<<<<<< Updated upstream
+=======
+        player.update(delta);
+        fountain.update(delta);
+        for(int i = 0; i < flags.size(); i++) {
+            flags.get(i).update(delta);
+        }
+>>>>>>> Stashed changes
     }
 
     @Override
@@ -66,6 +133,15 @@ public class GameScreen implements Screen {
         //spriteBatch.setProjectionMatrix(gameCam.combined);
         spriteBatch.begin();
         player.draw(spriteBatch);
+<<<<<<< Updated upstream
+=======
+        spriteBatch.setProjectionMatrix(gameCam.combined);
+        spriteBatch.draw(fountain, fountain.getX(), fountain.getY());
+        for(int i = 0; i < flags.size(); i++) {
+            spriteBatch.setProjectionMatrix(gameCam.combined);
+            spriteBatch.draw(flags.get(i), flags.get(i).getX(), flags.get(i).getY());
+        }
+>>>>>>> Stashed changes
         spriteBatch.end();
     }
 
---
+++ core/src/com/mygdx/game/WorldAnimations/Flag.java
@@ -0,0 +1,48 @@
+package com.mygdx.game.WorldAnimations;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+public class Flag extends Sprite {
+
+    private float stateTime;
+    private int FRAME_ROWS = 1;
+    private int FRAME_COLS = 5;
+    private Texture fountainSheet;
+    private TextureRegion[] fountainRegion;
+    private Animation<TextureRegion> fountainAnimation;
+
+    public Flag() {
+        fountainSheet = new Texture(Gdx.files.internal("Flag.png"));
+        stateTime = 0.0f;
+        this.setSize(16, 32);
+
+        TextureRegion[][] temp = TextureRegion.split(fountainSheet,
+                fountainSheet.getWidth() / FRAME_COLS,
+                fountainSheet.getHeight() / FRAME_ROWS);
+
+        fountainRegion = new TextureRegion[FRAME_ROWS * FRAME_COLS];
+        int index = 0;
+        for(int i = 0; i < FRAME_ROWS; i++) {
+            for(int j = 0; j < FRAME_COLS; j++) {
+                fountainRegion[index++] = temp[i][j];
+            }
+        }
+        fountainAnimation = new Animation<TextureRegion>(0.1f, fountainRegion);
+    }
+
+    public void update(float dt) {
+        setRegion(getFrame(dt));
+    }
+
+    public TextureRegion getFrame(float dt) {
+        TextureRegion region;
+        region = fountainAnimation.getKeyFrame(stateTime, true);
+        stateTime += dt;
+        return region;
+    }
+
+}
